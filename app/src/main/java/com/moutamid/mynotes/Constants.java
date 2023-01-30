34af9352c066885fe66227e3418d6b6beb07331f
Moutamid <lumianokia188@gmail.com>
2023-01-30 14:56:48 +0500

    initial commit

--- app/src/main/java/com/moutamid/mynotes/Constants.java
+++ app/src/main/java/com/moutamid/mynotes/Constants.java
@@ -4,4 +4,5 @@ public class Constants {
     public static final String NOTES_LIST = "notes_list";
 
     public static final String WEATHER_LIST = "WEATHER_LIST";
+    public static final String IS_FIRST_TIME = "IS_FIRST_TIME";
 }
--- app/src/main/java/com/moutamid/mynotes/activity/MainActivity.java
+++ app/src/main/java/com/moutamid/mynotes/activity/MainActivity.java
@@ -1,21 +1,35 @@
 package com.moutamid.mynotes.activity;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.request.RequestOptions;
 import com.fxn.stash.Stash;
 import com.kwabenaberko.openweathermaplib.model.common.Main;
 import com.moutamid.mynotes.Constants;
+import com.moutamid.mynotes.R;
 import com.moutamid.mynotes.databinding.ActivityMainBinding;
 import com.moutamid.mynotes.models.WeatherDataModel;
 
 import org.eazegraph.lib.models.ValueLinePoint;
 import org.eazegraph.lib.models.ValueLineSeries;
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -32,6 +46,7 @@ import java.util.Date;
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding b;
+    ArrayList<String> tasksArrayList = Stash.getArrayList(Constants.NOTES_LIST, String.class);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,11 +55,17 @@ public class MainActivity extends AppCompatActivity {
         b = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(b.getRoot());
 
+        if (tasksArrayList.size() == 0) {
+            tasksArrayList.add(getDateTime() + "\n" + " Hi, my name is Moutamid. so my first note is that my saturday was going pretty well until i realized it was sunday :(");
+        }
+
         initChart();
 
         b.notesBtn.setOnClickListener(v -> {
             startActivity(new Intent(MainActivity.this, NotesActivity.class));
         });
+
+        initRecyclerView();
 
     }
 
@@ -88,28 +109,64 @@ public class MainActivity extends AppCompatActivity {
 
                 JSONObject mainObject = jsonObject.getJSONObject("main");
 
+                JSONArray weatherArray = jsonObject.getJSONArray("weather");
+                JSONObject firstObject = weatherArray.getJSONObject(0);
+                String iconName = firstObject.getString("icon");
+
 //                Double maxTemp = mainObject.getDouble("temp_max");
                 float currentTemp = (float) mainObject.getDouble("temp");
 
+                runOnUiThread(() -> {
+                    b.currentTempTxt.setText(currentTemp + "");
+
+                    Glide.with(getApplicationContext())
+                            .load("http://openweathermap.org/img/wn/" + iconName + "@2x.png")
+                            .apply(new RequestOptions()
+                                    .error(R.drawable.clearsky)
+                            )
+                            .diskCacheStrategy(DiskCacheStrategy.DATA)
+                            .into(b.weatherIcon);
+                });
+
                 ValueLineSeries series = new ValueLineSeries();
-                series.setColor(0xFF56B7F1);
+//                series.setColor(0xFF56B7F1);
+                series.setColor(0xFFF6C15A);
 
                 ArrayList<WeatherDataModel> list = Stash.getArrayList(Constants.WEATHER_LIST, WeatherDataModel.class);
 
-                list.add(new WeatherDataModel(getCurrentDayName(), currentTemp));
+                // IF SIZE 0 THEN ADD ITEMS BEFORE
+                if (list.size() == 0) {
+                    list.add(new WeatherDataModel("", 0));
+                    list.add(new WeatherDataModel("", currentTemp / 2));
+                    list.add(new WeatherDataModel("", 0));
+                }
 
-                series.addPoint(new ValueLinePoint("", 0));
+                int count = 0;
+                for (WeatherDataModel weatherDataModel : list) {
+                    if (weatherDataModel.day.equals(getCurrentDayName())) {
+                        count++;
+                    }
+                }
+                if (count == 0)
+                    list.add(new WeatherDataModel(getCurrentDayName(), currentTemp));
+
+                // IF SIZE 4 THEN ADD ITEMS AFTER
+                if (list.size() == 4) {
+                    list.add(new WeatherDataModel("", 0));
+                    list.add(new WeatherDataModel("", currentTemp / 2));
+                    list.add(new WeatherDataModel("", 0));
+                }
 
                 for (int i = 0; i < list.size(); i++) {
                     series.addPoint(new ValueLinePoint(list.get(i).day, list.get(i).degree));
                 }
 
-                series.addPoint(new ValueLinePoint("", 0));
-
                 runOnUiThread(() -> {
                     b.cubiclinechart.addSeries(series);
                     b.cubiclinechart.startAnimation();
                 });
+
+                Stash.put(Constants.WEATHER_LIST, list);
 
             } catch (JSONException e) {
                 e.printStackTrace();
@@ -123,4 +180,118 @@ public class MainActivity extends AppCompatActivity {
         return f.format(new Date());
     }
 
+    private RecyclerView conversationRecyclerView;
+    private RecyclerViewAdapterMessages adapter;
+
+    private void initRecyclerView() {
+
+        conversationRecyclerView = findViewById(R.id.mainRv);
+        //conversationRecyclerView.addItemDecoration(new DividerItemDecoration(conversationRecyclerView.getContext(), DividerItemDecoration.VERTICAL));
+        adapter = new RecyclerViewAdapterMessages();
+        //        LinearLayoutManager layoutManagerUserFriends = new LinearLayoutManager(mContext, LinearLayoutManager.HORIZONTAL, false);
+        //    int numberOfColumns = 3;
+        //int mNoOfColumns = calculateNoOfColumns(getApplicationContext(), 50);
+        //  recyclerView.setLayoutManager(new GridLayoutManager(this, mNoOfColumns));
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        linearLayoutManager.setReverseLayout(true);
+        conversationRecyclerView.setLayoutManager(linearLayoutManager);
+        conversationRecyclerView.setHasFixedSize(true);
+        conversationRecyclerView.setNestedScrollingEnabled(false);
+
+        conversationRecyclerView.setAdapter(adapter);
+
+        //    if (adapter.getItemCount() != 0) {
+
+        //        noChatsLayout.setVisibility(View.GONE);
+        //        chatsRecyclerView.setVisibility(View.VISIBLE);
+
+        //    }
+
+    }
+
+    private class RecyclerViewAdapterMessages extends RecyclerView.Adapter
+            <RecyclerViewAdapterMessages.ViewHolderRightMessage> {
+
+        @NonNull
+        @Override
+        public RecyclerViewAdapterMessages.ViewHolderRightMessage onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.layout_item_notes, parent, false);
+            return new RecyclerViewAdapterMessages.ViewHolderRightMessage(view);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull final RecyclerViewAdapterMessages.ViewHolderRightMessage holder, int position1) {
+
+            int position = holder.getAdapterPosition();
+
+            holder.title.setText(tasksArrayList.get(position));
+
+            holder.title.setOnLongClickListener(new View.OnLongClickListener() {
+                @Override
+                public boolean onLongClick(View view) {
+                    new AlertDialog.Builder(MainActivity.this)
+                            .setTitle("Are you sure?")
+                            .setMessage("Do you really want to delete this note?")
+                            .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialogInterface, int i) {
+
+                                    tasksArrayList.remove(position);
+                                    adapter.notifyItemRemoved(position);
+                                    Stash.put(Constants.NOTES_LIST, tasksArrayList);
+
+                                    dialogInterface.dismiss();
+                                }
+                            })
+                            .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialogInterface, int i) {
+                                    dialogInterface.dismiss();
+                                }
+                            })
+                            .show();
+
+                    return false;
+                }
+            });
+
+        }
+
+        @Override
+        public int getItemCount() {
+            if (tasksArrayList == null)
+                return 0;
+            return tasksArrayList.size();
+        }
+
+        public class ViewHolderRightMessage extends RecyclerView.ViewHolder {
+
+            TextView title;
+
+            public ViewHolderRightMessage(@NonNull View v) {
+                super(v);
+                title = v.findViewById(R.id.titleTextview);
+
+            }
+        }
+
+    }
+
+    private String getDateTime() {
+        try {
+            Date date = new Date();
+            SimpleDateFormat sdf = new SimpleDateFormat("EEE, d MMM yyyy HH:mm:ss");
+//            SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
+            return sdf.format(date);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "Error";
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        initRecyclerView();
+    }
 }
